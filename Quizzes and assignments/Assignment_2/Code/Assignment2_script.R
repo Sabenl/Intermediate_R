41df7adcc978cb3880a2137cc75bfc29eacd808c
Sabina Enlund <sabina.enlund@ki.se>
2024-11-25 11:25:25 +0100

    ran scripts

--- Quizzes and assignments/Assignment_2/Code/Assignment2_script.R
+++ Quizzes and assignments/Assignment_2/Code/Assignment2_script.R
@@ -1,3 +1,4 @@
+
 #### Set WD to current assignment
 Ass_1_WD <- "/Users/k6-yjwyh2vnmc/Library/CloudStorage/OneDrive-KarolinskaInstitutet/KI Work/PhD stuff/Courses/Intermediate R/R_version_control/Github_connected/Quizzes and assignments/Assignment_1"
 Ass_2_WD <- "/Users/k6-yjwyh2vnmc/Library/CloudStorage/OneDrive-KarolinskaInstitutet/KI Work/PhD stuff/Courses/Intermediate R/R_version_control/Github_connected/Quizzes and assignments/Assignment_2"
@@ -6,6 +7,5 @@ setwd(Ass_2_WD)
 # initialize renv
 renv::init()
 
-
-
-
+# renv lockfile
+renv::snapshot()
