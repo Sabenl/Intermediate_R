610a1d41c541a9a317e7c596f471dd44ca3d5a83
Sabina Enlund <sabina.enlund@ki.se>
2024-11-27 09:54:38 +0100

    added plots

--- Quizzes and assignments/Tasks/Code/Script_ggplot_task.R
+++ Quizzes and assignments/Tasks/Code/Script_ggplot_task.R
@@ -45,7 +45,8 @@ scatter_plot <- ggplot(plot2, aes(x = area, y = poptotal)) + # area on x axis, t
   ylab("Population") + # changes y axis label
   xlab("Area") + # removes x axis label
   theme_bw() + 
-  ggtitle("Scatterplot \n Area vs Population") + # Adds plot title
+  labs(subtitle="Area vs Population",
+       title= "Scatterplot") + # Adds plot title
   scale_y_continuous(limits = c(0, 500000), labels = scales::number) + # changes y axis range and removes scientific numbering
   scale_x_continuous(limits = c(0.000, 0.100), breaks = c(0.000,0.025, 0.050, 0.075, 0.100)) # changes x axis range and where the ticks appear
 
@@ -57,7 +58,14 @@ ggsave(plot = scatter_plot, path = "../Quizzes and assignments/Tasks/Figures/", 
 # read the data
 plot3 <- read.csv("../Quizzes and assignments/Tasks/Data/data_plot3.csv", header = TRUE)
 
-# scatter but as factor?
+ggplot(plot3, aes(x = cty, y = hwy, size = displ)) + 
+  geom_point(color = "brown3") +
+  labs(subtitle="mpg: city vs highway mileage",
+       title= "Counts plot") + # Adds plot title
+  theme(legend.position = "none") # removes the legend box# Adds plot title
+
+
+# group by year for size??
 
 
 ######## 4 Divergent plot ########
@@ -65,8 +73,32 @@ plot3 <- read.csv("../Quizzes and assignments/Tasks/Data/data_plot3.csv", header
 # read the data
 plot4 <- read.csv("../Quizzes and assignments/Tasks/Data/data_plot4.csv", header = TRUE)
 
+# compute normalized mpg
+plot4$mpg_z <- round((plot4$mpg - mean(plot4$mpg))/sd(plot4$mpg), 2)
+# above / below avg flag
+plot4$mpg_type <- ifelse(plot4$mpg_z < 0, "below", "above")
+# sort
+plot4 <- plot4[order(plot4$mpg_z), ]
+# convert to factor to retain sorted order in plot
+plot4$car_name <- factor(plot4$car_name, levels = plot4$car_name)
 
 
+bar_plot <- ggplot(plot4, aes(y = car_name, x = mpg_z, label = mpg_z)) +
+  geom_bar(stat='identity', aes(fill = mpg_type)) +
+  scale_fill_manual(name="Mileage",
+                    labels = c("Above Average", "Below Average"),
+                    values = c("above"="#97C48D", "below"="#f8766d")) +
+  xlab("MPG (Z-score)") +
+  ylab("Car Name") + 
+  theme_bw()
+
+# save the plot as pdf
+ggsave(plot = bar_plot, path = "../Quizzes and assignments/Tasks/Figures/", device = "pdf", filename = "Plot4_bar.pdf")
+
+
+# dodge position by group (above/below av mileage)
+# split above/below mileage
+# calc mpg z-score
 
 
 
